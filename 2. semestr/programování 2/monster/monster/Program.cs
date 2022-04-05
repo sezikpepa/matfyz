543a307b3924fe46e28f3f77d50a36a62d479ed8
Josef Sezemsky <sezikpepak@gmail.com>
2022-04-05 03:15:03 +0200

    Monster in Maze final solution - fixed severe bad code practise

--- 2. semestr/programování 2/monster/monster/Program.cs
+++ 2. semestr/programování 2/monster/monster/Program.cs
@@ -19,37 +19,17 @@ namespace MyApp // Note: actual namespace depends on the project name.
 
     class Maze
     {
-
         public string[,] places;
         public int width;
         public int height;
-        public Monster monster;
+        public List<Monster> monsters; //there is a possibility I would need more monsters in the maze - this extension would be much easier this way
 
         public Maze(int x, int y)
         {
             this.places = new string[x, y];
             this.width = x;
             this.height = y;
-        }
-
-        public void newMonster(int x, int y, string direction)
-        {
-            this.monster = new Monster(x, y);
-            switch (direction)
-            {
-                case "^":
-                    monster.direction = 0;
-                    break;
-                case ">":
-                    monster.direction = 1;
-                    break;
-                case "v":
-                    monster.direction = 2;
-                    break;
-                case "<":
-                    monster.direction = 3;
-                    break;
-            }
+            this.monsters = new List<Monster>();
         }
 
         public void mazeStructureChange(int x, int y, string input)
@@ -57,12 +37,10 @@ namespace MyApp // Note: actual namespace depends on the project name.
             if (input == "X" || input == ".")
             {
                 this.places[x, y] = input;
+                return;
             }
-            else
-            {
-                newMonster(x, y, input);
-            }
-            
+            Monster monster = new Monster(x, y, input);
+            this.monsters.Add(monster);
         }
 
         public void printMaze()
@@ -78,50 +56,25 @@ namespace MyApp // Note: actual namespace depends on the project name.
             Console.WriteLine();
         }
 
-        public void moveMonster()
+        public void moveMonsters()
         {
-            this.places[this.monster.position.x, this.monster.position.y] = ".";
-            int xShift = 0;
-            int yShift = 0;
-            if (this.monster.direction == 0 || this.monster.direction == 2)
+            foreach (Monster monster in this.monsters)
             {
-                yShift = this.monster.direction - 1;
+                this.moveMonster(monster);
             }
-            else if (this.monster.direction == 1 || this.monster.direction == 3)
-            {
-                xShift = -(this.monster.direction - 2);
-            }
+        }
+        public void moveMonster(Monster monster)
+        {
+            this.places[monster.position.x, monster.position.y] = ".";
 
+            monster.move(this.places);
+            this.displayNewMonsterState(monster);
 
-            if (this.places[this.monster.position.x - yShift, this.monster.position.y + xShift] == "." && !this.monster.forceForward)
-            {
-                this.monster.direction += 1;
-                this.monster.direction %= 4;
-                this.monster.forceForward = true;
-            }
+        }
 
-            else if (this.places[this.monster.position.x + xShift, this.monster.position.y + yShift] == ".")
-            {
-                this.monster.position.x += xShift;
-                this.monster.position.y += yShift;
-                this.monster.forceForward = false;
-            }
-
-            
-            else if (this.places[this.monster.position.x + yShift, this.monster.position.y - xShift] == ".")
-            {
-                this.monster.direction -= 1;
-                this.monster.direction %= 4;
-            }
-
-            else
-            {
-                this.monster.direction -= 1;
-                this.monster.direction %= 4;
-            }
-            
-            this.places[this.monster.position.x, this.monster.position.y] = this.monster.displayCharacter();
-            
+        private void displayNewMonsterState(Monster monster)
+        {
+            this.places[monster.position.x, monster.position.y] = monster.displayCharacter();
         }
     }
 
@@ -129,10 +82,11 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         public Coordinate position;
         public int direction;
-        public Dictionary<int, string> positionsRepresentation;
-        public bool forceForward;
+        private Dictionary<int, string> positionsRepresentation;
+        private Dictionary<string, int> positionsRepresentationReverse;
+        private bool forceForward;
 
-        public Monster(int x, int y)
+        public Monster(int x, int y, string direction)
         {
             this.position = new Coordinate(x, y);
             this.positionsRepresentation = new Dictionary<int, string>();
@@ -140,7 +94,67 @@ namespace MyApp // Note: actual namespace depends on the project name.
             this.positionsRepresentation.Add(1, ">");
             this.positionsRepresentation.Add(2, "v");
             this.positionsRepresentation.Add(3, "<");
+
+            this.positionsRepresentationReverse = new Dictionary<string, int>();
+            this.positionsRepresentationReverse.Add("^", 0);
+            this.positionsRepresentationReverse.Add(">", 1);
+            this.positionsRepresentationReverse.Add("v", 2);
+            this.positionsRepresentationReverse.Add("<", 3);
+
+            this.direction = this.positionsRepresentationReverse[direction];
             this.forceForward = false;
+        }
+
+        public void move(string[,] maze)
+        {
+            int[] shifts = new int[2];
+            shifts = this.generateShiftsForNextMove();
+            int xShift = shifts[0];
+            int yShift = shifts[1];
+
+            if (maze[this.position.x - yShift, this.position.y + xShift] == "." && !this.forceForward)
+            {
+                this.direction += 1;
+                this.direction %= 4;
+                this.forceForward = true;
+                return;
+            }
+
+            if (maze[this.position.x + xShift, this.position.y + yShift] == ".")
+            {
+                this.position.x += xShift;
+                this.position.y += yShift;
+                this.forceForward = false;
+                return;
+            }
+
+
+            if (maze[this.position.x + yShift, this.position.y - xShift] == ".")
+            {
+                this.direction -= 1;
+                this.direction %= 4;
+                return;
+            }
+
+            this.direction -= 1;
+            this.direction %= 4;
+        }
+
+
+        private int[] generateShiftsForNextMove()
+        {
+            int xShift = 0;
+            int yShift = 0;
+            if (this.direction == 0 || this.direction == 2)
+            {
+                yShift = this.direction - 1;
+            }
+            else if (this.direction == 1 || this.direction == 3)
+            {
+                xShift = -(this.direction - 2);
+            }
+            int[] returnValue = new int[] { xShift, yShift };
+            return returnValue;
         }
 
         public void normalizeDirection()
@@ -162,54 +176,36 @@ namespace MyApp // Note: actual namespace depends on the project name.
     internal class Program
     {
 
-        static void Main(string[] args)
+        static void readInputs(Maze maze, int width, int height)
         {
-            //int width = Int32.Parse(Console.ReadLine());
-            //int height = Int32.Parse(Console.ReadLine());
-
-            int width = 10;
-            int height = 6;
-
-            Maze maze = new Maze(width, height);
-
-            /*
-            string[,] places = new string[,] { { "1", "2", "3", "4", "5", "6"},
-                                               { "7", ".", ".", ".", ".", "X"},
-                                               { "X", ".", ".", ".", ".", "X"},
-                                               { "X", ".", "X", ".", ".", "X"},
-                                               { "X", ".", "X", ".", ">", "."},
-                                               { "X", "X", "X", "X", "X", "X"}};
-            */
-            
-             string[,] places = new string[,] { { "X", "X", "X", "X", "X", "X"},
-                                               { "X", ".", ".", ".", ".", "X"},
-                                               { "X", ".", ".", "X", "X", "X"},
-                                               { "X", ".", ".", ".", ".", "X"},
-                                               { "X", ".", ".", "^", ".", "X"},
-                                               { "X", "X", "X", "X", ".", "X"},
-                                               { "X", ".", ".", ".", ".", "X"},
-                                               { "X", ".", ".", "X", "X", "X"},
-                                               { "X", ".", ".", ".", ".", "X"},
-                                               { "X", "X", "X", "X", "X", "X"}};
-
-
-            /*
             for (int y = 0; y < height; y++)
             {
                 string newLine = Console.ReadLine();
                 for (int x = 0; x < width; x++)
                 {
-                    maze.places[x, y] = Char.ToString(newLine[x]);
+                    maze.mazeStructureChange(x, y, Char.ToString(newLine[x]));
                 }
             }
-            */
-            maze.places = places;
-            maze.mazeStructureChange(4, 3, "^");
+        }
+
+        static void moveMonsterstwentyTimes(Maze maze)
+        {
             for (int i = 0; i < 20; i++)
             {
-                maze.moveMonster();
+                maze.moveMonsters();
                 maze.printMaze();
             }
         }
-    }   
+
+        static void Main(string[] args)
+        {
+            int width = Int32.Parse(Console.ReadLine());
+            int height = Int32.Parse(Console.ReadLine());
+
+            Maze maze = new Maze(width, height);
+
+            readInputs(maze, width, height);
+            moveMonsterstwentyTimes(maze);     
+        }
+    }
 }
