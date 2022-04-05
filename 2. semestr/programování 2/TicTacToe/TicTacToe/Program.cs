a1153463b0c56a18c2ed6b64167ab40b4d24c8ec
Josef Sezemsky <sezikpepak@gmail.com>
2022-04-05 12:35:25 +0200

    basic TicTacToe structure

---
+++ 2. semestr/programování 2/TicTacToe/TicTacToe/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Board
+    {
+        public string[,] fields;
+
+        public Board(string[,] fields)
+        {
+            this.fields = new string[3,3];
+            this.fields = fields;
+        }
+
+        public void print()
+        {
+            for (int x = 0; x < 3; x++)
+            {
+                for(int y = 0; y < 3; y++)
+                {
+                    Console.Write(this.fields[x,y]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+
+    class Player
+    {
+        public string symbol;
+
+        public Player(string symbol)
+        {
+            this.symbol = symbol;
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
