081a6a8f462a59e5ec985e4a719948f20c720c47
Josef Sezemsky <sezikpepak@gmail.com>
2022-04-13 15:15:28 +0200

    YoungTable project create

---
+++ 2. semestr/programování 2/YoungTables/YoungTables/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Int32.Parse(Console.ReadLine());
+
+            int[,] table = new int[number, number];
+            
+
+
+        }
+    }
+}
