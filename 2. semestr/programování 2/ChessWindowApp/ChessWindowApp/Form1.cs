a2bc2d4429ba7c45b9b422882f8c3fbefb119d8b
Josef Sezemsky <sezikpepak@gmail.com>
2022-09-05 18:53:09 +0200

    gui done

--- 2. semestr/programování 2/ChessWindowApp/ChessWindowApp/Form1.cs
+++ 2. semestr/programování 2/ChessWindowApp/ChessWindowApp/Form1.cs
@@ -16,5 +16,15 @@ namespace ChessWindowApp
         {
 
         }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
